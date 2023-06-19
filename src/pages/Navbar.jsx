034899b19f0d5fc5aef623dafbcf4d73e422537b
Zosia <zosiagrafika11@gmail.com>
2023-06-19 21:55:08 +0200

    navbar style

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
+import { FaRegMoon } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/expenses"}>Expenses</Link>
-        <p>test</p>
+    <nav className=" font-K2D p-7 flex items-end justify-between ">
+      <h1 className=" text-4xl font-bold">ExpenseManager</h1>
+      <ul className="flex  items-center gap-8 text-xl">
+        <Link to={"/"} className=" font-semibold hover:scale-110 hover:text-green-600">
+          Home
+        </Link>
+        <Link to={"/expenses"} className=" font-semibold hover:scale-110 hover:text-green-600">
+          Expenses
+        </Link>
+        <FaRegMoon size={18} className="cursor-pointer hover:scale-125" />
       </ul>
     </nav>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        K2D: ["K2D", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
