13c3e470ec31e6f0a4f53f41fecd3ea000ac671b
Zosia <zosiagrafika11@gmail.com>
2023-06-20 19:52:54 +0200

    navbar and menu functionality

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,8 +1,16 @@
 import { Link } from "react-router-dom";
 import { FaRegMoon } from "react-icons/fa";
 import { FiMenu } from "react-icons/fi";
+import { RxCross2 } from "react-icons/rx";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isIconMenuOrClose, setIsIconMenuOrClose] = useState(true);
+
+  const toggleIconsMenu = () => {
+    setIsIconMenuOrClose(!isIconMenuOrClose);
+  };
+
   return (
     <section>
       <nav className=" font-K2D p-7 flex items-end justify-between ">
@@ -17,24 +25,39 @@ const Navbar = () => {
           <FaRegMoon size={18} className="cursor-pointer hover:scale-125  hover:text-green-600" />
         </ul>
 
-        <FiMenu size={30} className="md:hidden" />
+        {isIconMenuOrClose ? (
+          <RxCross2
+            onClick={toggleIconsMenu}
+            size={30}
+            className="relative md:hidden z-50 text-white"
+          />
+        ) : (
+          <FiMenu onClick={toggleIconsMenu} size={30} className="relative md:hidden z-50" />
+        )}
       </nav>
 
-      <aside className="md:hidden">
-        <div className=" fixed right-0 top-0 w-[30%] h-full text-white bg-black opacity-90">
-          <ul className="flex flex-col justify-center items-center py-8 mt-10 gap-6 text-xl ">
-            <Link to={"/"} className=" font-semibold hover:scale-110 hover:text-green-600">
-              Home
-            </Link>
-            <Link
-              to={"/expenses"}
-              className=" block font-semibold hover:scale-110 hover:text-green-600"
-            >
-              Expenses
-            </Link>
-            <FaRegMoon size={18} className="cursor-pointer hover:scale-125  hover:text-green-600" />
-          </ul>
-        </div>
+      <aside
+        className={
+          isIconMenuOrClose
+            ? "fixed right-0 top-0 w-[30%] h-full text-white bg-black opacity-90 md:hidden "
+            : "hidden"
+        }
+      >
+        <ul
+          className="flex flex-col justify-center items-center py-8 mt-10
+          gap-6 text-xl "
+        >
+          <Link to={"/"} className=" font-semibold hover:scale-110 hover:text-green-600">
+            Home
+          </Link>
+          <Link
+            to={"/expenses"}
+            className=" block font-semibold hover:scale-110 hover:text-green-600"
+          >
+            Expenses
+          </Link>
+          <FaRegMoon size={18} className="cursor-pointer hover:scale-125  hover:text-green-600" />
+        </ul>
       </aside>
     </section>
   );
