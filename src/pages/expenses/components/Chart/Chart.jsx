834ed584e373475c35206938f06eec6a66e10d5f
Zosia <zosiagrafika11@gmail.com>
2023-07-04 22:26:40 +0200

    adding buttons to the chart

--- src/pages/expenses/components/Chart/Chart.jsx
+++ src/pages/expenses/components/Chart/Chart.jsx
@@ -4,25 +4,41 @@ import Context from "../../../../Context";
 import { Chart as ChartJS } from "chart.js/auto";
 
 const Chart = () => {
-  const { historyData } = useContext(Context);
+  const { mode, historyData } = useContext(Context);
 
-  const sortedDates = historyData
-    .map((data) => data.incomesDate)
-    .sort((a, b) => new Date(a) - new Date(b));
+  const sortedData = historyData
+    .slice()
+    .sort((a, b) => new Date(a.incomesDate) - new Date(b.incomesDate));
+
+  const Dates = sortedData.map((data) => data.incomesDate);
+
+  const incomesAmounts = sortedData.map((data) => data.incomesAmount);
 
   const chartData = {
-    labels: sortedDates,
+    labels: Dates,
     datasets: [
       {
         label: "Income",
-        data: historyData.map((data) => data.incomesAmount),
+        data: incomesAmounts,
+        borderColor: "rgb(55, 138, 63)",
+        backgroundColor: "rgb(90, 153, 96)",
       },
     ],
   };
 
   return (
-    <div className="w-[600px] mt-12">
-      <Line data={chartData} />
+    <div className=" font-K2D">
+      <div className="max-w-[600px] lg:max-w-[800px] lg:mx-auto mt-16">
+        <div
+          className={`flex justify-between text-lg sm:text-2xl font-semibold border-b-2 mb-8 ${
+            mode === "light" ? "border-black" : "border-white"
+          }`}
+        >
+          <button>Incomes chart</button>
+          <button>Expenses chart</button>
+        </div>
+        <Line data={chartData} />
+      </div>
     </div>
   );
 };
