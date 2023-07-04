60b0f107ee9f7104a6450df0e1a31f49d335fb5b
Zosia <zosiagrafika11@gmail.com>
2023-07-04 23:25:29 +0200

    adding charts

--- src/pages/expenses/components/Chart/Chart.jsx
+++ src/pages/expenses/components/Chart/Chart.jsx
@@ -1,27 +1,62 @@
 import { Line } from "react-chartjs-2";
 import { useContext } from "react";
 import Context from "../../../../Context";
+import { useState } from "react";
 import { Chart as ChartJS } from "chart.js/auto";
 
 const Chart = () => {
   const { mode, historyData } = useContext(Context);
+  const [incomesChartOpen, setIncomesChartOpen] = useState(true);
+  const [expensesChartOpen, setExpensesChartOpen] = useState(false);
 
-  const sortedData = historyData
+  const changeBackgroundColor = mode === "light" ? "bg-[#F3F3F3]" : "bg-[#272626]";
+
+  const openIncomes = () => {
+    setIncomesChartOpen(!incomesChartOpen);
+  };
+
+  const openExpenses = () => {
+    setExpensesChartOpen(!expensesChartOpen);
+  };
+
+  const sortedIncomesData = historyData
+    .filter((data) => data.hasOwnProperty("incomesAmount"))
     .slice()
     .sort((a, b) => new Date(a.incomesDate) - new Date(b.incomesDate));
 
-  const Dates = sortedData.map((data) => data.incomesDate);
+  const datesIncomes = sortedIncomesData.map((data) => data.incomesDate);
 
-  const incomesAmounts = sortedData.map((data) => data.incomesAmount);
+  const incomesAmounts = sortedIncomesData.map((data) => data.incomesAmount);
 
-  const chartData = {
-    labels: Dates,
+  const chartIncomesData = {
+    labels: datesIncomes,
     datasets: [
       {
         label: "Income",
         data: incomesAmounts,
         borderColor: "rgb(55, 138, 63)",
         backgroundColor: "rgb(90, 153, 96)",
+      },
+    ],
+  };
+
+  const sortedExpensesData = historyData
+    .filter((data) => data.hasOwnProperty("expensesAmount"))
+    .slice()
+    .sort((a, b) => new Date(a.expensesDate) - new Date(b.expensesDate));
+
+  const datesExpenses = sortedExpensesData.map((data) => data.expensesDate);
+
+  const expensesAmounts = sortedExpensesData.map((data) => data.expensesAmount);
+
+  const charExpensesData = {
+    labels: datesExpenses,
+    datasets: [
+      {
+        label: "Expense",
+        data: expensesAmounts,
+        borderColor: "rgb(173, 21, 16)",
+        backgroundColor: "rgb(219, 45, 39)",
       },
     ],
   };
@@ -34,10 +69,21 @@ const Chart = () => {
             mode === "light" ? "border-black" : "border-white"
           }`}
         >
-          <button>Incomes chart</button>
-          <button>Expenses chart</button>
+          <button
+            onClick={openIncomes}
+            className={`${changeBackgroundColor} mb-2 p-2 rounded-xl text-lg font-semibold hover:bg-[#B0E2C7] hover:text-black`}
+          >
+            Incomes chart
+          </button>
+          <button
+            onClick={openExpenses}
+            className={`${changeBackgroundColor} mb-2 p-2 rounded-xl text-lg font-semibold hover:bg-[#FEC7C7] hover:text-black`}
+          >
+            Expenses chart
+          </button>
         </div>
-        <Line data={chartData} />
+        {incomesChartOpen && <Line data={chartIncomesData} />}
+        {expensesChartOpen && <Line data={charExpensesData} />}
       </div>
     </div>
   );
