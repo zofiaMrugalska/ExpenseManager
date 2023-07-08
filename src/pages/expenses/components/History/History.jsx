c3799bad167e5ab42321537d5ac9b76712834068
Zosia <zosiagrafika11@gmail.com>
2023-07-08 17:06:22 +0200

    map component edit functionality

--- src/pages/expenses/components/History/History.jsx
+++ src/pages/expenses/components/History/History.jsx
@@ -1,9 +1,8 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Context from "../../../../Context";
-import { useState } from "react";
-import { HiOutlineTrash } from "react-icons/hi";
 
 import Filter from "./Filter";
+import MapHistory from "./MapHistory";
 
 const History = () => {
   const { mode, historyData, setHistoryData } = useContext(Context);
@@ -11,10 +10,6 @@ const History = () => {
   const [filterIncomes, setFilterIncomes] = useState(true);
   const [filterExpenses, setFilterExpenses] = useState(true);
   const [filterAll, setFilterAll] = useState(true);
-
-  const [editBtn, setEditBtn] = useState(true);
-
-  const changeBackgroundColor = mode === "light" ? "bg-[#F3F3F3]" : "bg-[#272626]";
 
   const sortedHistoryData = historyData.sort((a, b) => {
     const dateA = new Date(a.expensesDate || a.incomesDate);
@@ -27,12 +22,6 @@ const History = () => {
     newHistoryData.splice(index, 1);
     setHistoryData(newHistoryData);
   };
-
-  const toggleEditBtn = () => {
-    setEditBtn(!editBtn);
-  };
-
-  const handleChangeInput = (event) => {};
 
   return (
     <div className=" font-K2D">
@@ -53,121 +42,17 @@ const History = () => {
         </div>
 
         <div className="max-w-[600px] max-h-[300px] overflow-auto">
-          {sortedHistoryData.map((value, index) => {
-            return (
-              <div key={index}>
-                {value.incomesTitle && (
-                  <div
-                    className={`grid grid-cols-5 place-items-center mt-5 py-3 px-3 rounded-xl text-base sm:text-lg ${changeBackgroundColor} ${
-                      filterIncomes ? `block` : `hidden`
-                    } `}
-                  >
-                    {editBtn ? (
-                      <p>{value.incomesTitle}</p>
-                    ) : (
-                      <input
-                        name="incomesTitle"
-                        value={value.incomesTitle}
-                        onChange={handleChangeInput}
-                        className=" max-w-[110px] border border-black rounded-lg"
-                      />
-                    )}
-
-                    {editBtn ? (
-                      <p>{value.incomesDate}</p>
-                    ) : (
-                      <input
-                        name="incomesDate"
-                        value={value.incomesDate}
-                        onChange={handleChangeInput}
-                        type="date"
-                        className=" max-w-[110px] border border-black rounded-lg"
-                      />
-                    )}
-
-                    {editBtn ? (
-                      <p className=" text-green-600 font-semibold">{value.incomesAmount}$</p>
-                    ) : (
-                      <input
-                        name="incomesAmount"
-                        value={value.incomesAmount}
-                        onChange={handleChangeInput}
-                        type="number"
-                        className=" max-w-[110px] border border-black rounded-lg"
-                      />
-                    )}
-
-                    <HiOutlineTrash
-                      onClick={() => deleteItem(index)}
-                      size={23}
-                      className=" cursor-pointer"
-                    />
-                    <button
-                      onClick={toggleEditBtn}
-                      className=" hover:scale-110 hover:font-semibold"
-                    >
-                      edit
-                    </button>
-                  </div>
-                )}
-
-                {value.expensesTitle && (
-                  <div
-                    className={`grid grid-cols-5 place-items-center mt-5 py-3 px-3 rounded-xl text-base sm:text-lg ${changeBackgroundColor} ${
-                      filterExpenses ? "block" : "hidden"
-                    }`}
-                  >
-                    {editBtn ? (
-                      <p>{value.expensesTitle}</p>
-                    ) : (
-                      <input
-                        name="expensesTitle"
-                        value={value.expensesTitle}
-                        onChange={handleChangeInput}
-                        className=" max-w-[110px] border border-black rounded-lg"
-                      />
-                    )}
-
-                    {editBtn ? (
-                      <p>{value.expensesDate}</p>
-                    ) : (
-                      <input
-                        name="expensesDate"
-                        value={value.expensesDate}
-                        onChange={handleChangeInput}
-                        type="date"
-                        className=" max-w-[110px] border border-black  rounded-lg"
-                      />
-                    )}
-
-                    {editBtn ? (
-                      <p className=" text-red-600 font-semibold">{value.expensesAmount}$</p>
-                    ) : (
-                      <input
-                        name="expensesAmount"
-                        value={value.expensesAmount}
-                        onChange={handleChangeInput}
-                        type="number"
-                        className=" max-w-[110px] border border-black rounded-lg"
-                      />
-                    )}
-
-                    <HiOutlineTrash
-                      onClick={() => deleteItem(index)}
-                      size={23}
-                      className=" cursor-pointer"
-                    />
-                    <button
-                      onClick={toggleEditBtn}
-                      className=" hover:scale-110 hover:font-semibold"
-                    >
-                      edit
-                    </button>
-                  </div>
-                )}
-              </div>
-            );
-          })}
+          {sortedHistoryData.map((value, index) => (
+            <div key={index}>
+              <MapHistory
+                value={value}
+                index={index}
+                filterIncomes={filterIncomes}
+                filterExpenses={filterExpenses}
+                deleteItem={deleteItem}
+              />
+            </div>
+          ))}
         </div>
       </section>
     </div>
