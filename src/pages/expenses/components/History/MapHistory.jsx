c2edbc545b27d0a8bd3ecdfff26504a75d23aecf
Zosia <zosiagrafika11@gmail.com>
2023-07-09 17:52:51 +0200

    editing by react hook form

--- src/pages/expenses/components/History/MapHistory.jsx
+++ src/pages/expenses/components/History/MapHistory.jsx
@@ -1,5 +1,6 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import Context from "../../../../Context";
+import { useForm } from "react-hook-form";
 
 import { HiOutlineTrash } from "react-icons/hi";
 
@@ -7,39 +8,10 @@ const MapHistory = ({ value, index, filterIncomes, filterExpenses, deleteItem })
   const { mode, historyData, setHistoryData } = useContext(Context);
 
   const [editBtn, setEditBtn] = useState(true);
-  const [editingData, setEditingData] = useState({ ...historyData[index] });
+
+  const { register, handleSubmit, reset } = useForm();
 
   const changeBackgroundColor = mode === "light" ? "bg-[#F3F3F3]" : "bg-[#272626]";
-
-  const toggleEditBtn = () => {
-    setEditBtn(!editBtn);
-  };
-
-  const handleChangeInput = (event) => {
-    const { name, value } = event.target;
-
-    if (name === "incomesAmount" || name === "expensesAmount") {
-      setEditingData((previousData) => ({
-        ...previousData,
-        [name]: Number(value),
-      }));
-    } else {
-      setEditingData((previousData) => ({
-        ...previousData,
-        [name]: value,
-      }));
-    }
-  };
-
-  const handleSave = () => {
-    setHistoryData((previousList) => {
-      const newList = [...previousList];
-      newList[index] = editingData;
-      return newList;
-    });
-    toggleEditBtn();
-    console.log(historyData);
-  };
 
   const thereIsIncomesData = () => {
     return value.incomesAmount || value.incomesDate || value.incomesTitle;
@@ -49,117 +21,158 @@ const MapHistory = ({ value, index, filterIncomes, filterExpenses, deleteItem })
     return value.expensesAmount || value.expensesDate || value.expensesTitle;
   };
 
+  const toggleEditBtn = () => {
+    setEditBtn(!editBtn);
+  };
+
+  const onSubmit = (data) => {
+    toggleEditBtn();
+    let newList = [...historyData];
+    newList[index] = data;
+    setHistoryData(newList);
+    reset();
+  };
+
   return (
     <div key={index}>
-      {thereIsIncomesData() && (
-        <div
-          className={`grid grid-cols-5 place-items-center mt-5 py-3 px-3 rounded-xl text-base sm:text-lg ${changeBackgroundColor} ${
-            filterIncomes ? `block` : `hidden`
-          } `}
-        >
-          {editBtn ? (
-            <p>{value.incomesTitle}</p>
-          ) : (
-            <input
-              name="incomesTitle"
-              value={editingData.incomesTitle}
-              onChange={handleChangeInput}
-              className=" max-w-[110px] border border-black rounded-lg"
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {thereIsIncomesData() && (
+          <div
+            className={`grid grid-cols-5 place-items-center mt-5 py-3 px-3 rounded-xl text-base sm:text-lg ${changeBackgroundColor} ${
+              filterIncomes ? `block` : `hidden`
+            } `}
+          >
+            {editBtn ? (
+              <p>{value.incomesTitle}</p>
+            ) : (
+              <input
+                {...register("incomesTitle", {
+                  required: "title is required",
+                  minLength: { value: 1, message: "no less than 4 characters" },
+                  maxLength: { value: 20, message: "no more than 20 characters" },
+                })}
+                type="text"
+                placeholder={value.incomesTitle}
+                className="max-w-[110px] border border-black rounded-lg"
+              />
+            )}
+
+            {editBtn ? (
+              <p>{value.incomesDate}</p>
+            ) : (
+              <input
+                {...register("incomesDate", {
+                  required: "title is required",
+                  minLength: { value: 1, message: "no less than 4 characters" },
+                  maxLength: { value: 20, message: "no more than 20 characters" },
+                })}
+                type="date"
+                className="max-w-[110px] border border-black rounded-lg"
+              />
+            )}
+
+            {editBtn ? (
+              <p className=" text-green-600 font-semibold">{value.incomesAmount}$</p>
+            ) : (
+              <input
+                {...register("incomesAmount", {
+                  required: "title is required",
+                  minLength: { value: 1, message: "no less than 4 characters" },
+                  maxLength: { value: 20, message: "no more than 20 characters" },
+                })}
+                type="number"
+                placeholder={value.incomesAmount}
+                className="max-w-[110px] border border-black rounded-lg"
+              />
+            )}
+
+            <HiOutlineTrash
+              onClick={() => deleteItem(index)}
+              size={23}
+              className=" cursor-pointer"
             />
-          )}
 
-          {editBtn ? (
-            <p>{value.incomesDate}</p>
-          ) : (
-            <input
-              name="incomesDate"
-              value={editingData.incomesDate}
-              onChange={handleChangeInput}
-              type="date"
-              className=" max-w-[110px] border border-black rounded-lg"
+            {editBtn ? (
+              <button onClick={toggleEditBtn} className=" hover:scale-110 hover:font-semibold">
+                edit
+              </button>
+            ) : (
+              <button onSubmit={onSubmit} className=" hover:scale-110 hover:font-semibold">
+                save
+              </button>
+            )}
+          </div>
+        )}
+
+        {thereIsExpensesData() && (
+          <div
+            className={`grid grid-cols-5 place-items-center mt-5 py-3 px-3 rounded-xl text-base sm:text-lg ${changeBackgroundColor} ${
+              filterExpenses ? "block" : "hidden"
+            }`}
+          >
+            {editBtn ? (
+              <p>{value.expensesTitle}</p>
+            ) : (
+              <input
+                {...register("expensesTitle", {
+                  required: "title is required",
+                  minLength: { value: 1, message: "no less than 4 characters" },
+                  maxLength: { value: 20, message: "no more than 20 characters" },
+                })}
+                type="text"
+                placeholder={value.expensesTitle}
+                className="max-w-[110px] border border-black rounded-lg"
+              />
+            )}
+
+            {editBtn ? (
+              <p>{value.expensesDate}</p>
+            ) : (
+              <input
+                {...register("expensesDate", {
+                  required: "title is required",
+                  minLength: { value: 1, message: "no less than 4 characters" },
+                  maxLength: { value: 20, message: "no more than 20 characters" },
+                })}
+                type="date"
+                placeholder={value.expensesDate}
+                className="max-w-[110px] border border-black rounded-lg"
+              />
+            )}
+
+            {editBtn ? (
+              <p className=" text-red-600 font-semibold">{value.expensesAmount}$</p>
+            ) : (
+              <input
+                {...register("expensesAmount", {
+                  required: "title is required",
+                  minLength: { value: 1, message: "no less than 4 characters" },
+                  maxLength: { value: 20, message: "no more than 20 characters" },
+                })}
+                type="number"
+                placeholder={value.expensesAmount}
+                className="max-w-[110px] border border-black rounded-lg"
+              />
+            )}
+
+            <HiOutlineTrash
+              onClick={() => deleteItem(index)}
+              size={23}
+              className=" cursor-pointer"
             />
-          )}
 
-          {editBtn ? (
-            <p className=" text-green-600 font-semibold">{value.incomesAmount}$</p>
-          ) : (
-            <input
-              name="incomesAmount"
-              value={editingData.incomesAmount}
-              onChange={handleChangeInput}
-              type="number"
-              className=" max-w-[110px] border border-black rounded-lg"
-            />
-          )}
-
-          <HiOutlineTrash onClick={() => deleteItem(index)} size={23} className=" cursor-pointer" />
-
-          {editBtn ? (
-            <button onClick={toggleEditBtn} className=" hover:scale-110 hover:font-semibold">
-              edit
-            </button>
-          ) : (
-            <button onClick={handleSave} className=" hover:scale-110 hover:font-semibold">
-              save
-            </button>
-          )}
-        </div>
-      )}
-
-      {thereIsExpensesData() && (
-        <div
-          className={`grid grid-cols-5 place-items-center mt-5 py-3 px-3 rounded-xl text-base sm:text-lg ${changeBackgroundColor} ${
-            filterExpenses ? "block" : "hidden"
-          }`}
-        >
-          {editBtn ? (
-            <p>{value.expensesTitle}</p>
-          ) : (
-            <input
-              name="expensesTitle"
-              value={editingData.expensesTitle}
-              onChange={handleChangeInput}
-              className=" max-w-[110px] border border-black rounded-lg"
-            />
-          )}
-
-          {editBtn ? (
-            <p>{value.expensesDate}</p>
-          ) : (
-            <input
-              name="expensesDate"
-              value={editingData.expensesDate}
-              onChange={handleChangeInput}
-              type="date"
-              className=" max-w-[110px] border border-black  rounded-lg"
-            />
-          )}
-
-          {editBtn ? (
-            <p className=" text-red-600 font-semibold">{value.expensesAmount}$</p>
-          ) : (
-            <input
-              name="expensesAmount"
-              value={editingData.expensesAmount}
-              onChange={handleChangeInput}
-              type="number"
-              className=" max-w-[110px] border border-black rounded-lg"
-            />
-          )}
-
-          <HiOutlineTrash onClick={() => deleteItem(index)} size={23} className=" cursor-pointer" />
-
-          {editBtn ? (
-            <button onClick={toggleEditBtn} className=" hover:scale-110 hover:font-semibold">
-              edit
-            </button>
-          ) : (
-            <button onClick={handleSave} className=" hover:scale-110 hover:font-semibold">
-              save
-            </button>
-          )}
-        </div>
-      )}
+            {editBtn ? (
+              <button onClick={toggleEditBtn} className=" hover:scale-110 hover:font-semibold">
+                edit
+              </button>
+            ) : (
+              <button onSubmit={onSubmit} className=" hover:scale-110 hover:font-semibold">
+                save
+              </button>
+            )}
+          </div>
+        )}
+      </form>
     </div>
   );
 };
